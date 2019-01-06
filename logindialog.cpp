d4bf4c49aab875f37fcc29e52c9d6f246cc05d7c
liolok <liolokit@gmail.com>
2019-01-07 00:25:57 +0800

    use `arg()` formatting query statements
    It's more suitable than `replace()` in this situation.

--- logindialog.cpp
+++ logindialog.cpp
@@ -11,22 +11,22 @@ LoginDialog::LoginDialog(QWidget *parent) :
     ui->lineEditUsername->setMaxLength(USERNAME_MAX_LEN);
     ui->lineEditPassword->setMaxLength(PASSWORD_MAX_LEN);
 
-    // If user table doesn't exist yet, create it.
-    QString create_user_table = "CREATE TABLE IF NOT EXISTS Users ("
-        "name char(name_len) PRIMARY KEY, pswd char(pswd_len))";
-    create_user_table.replace("name_len", QString::number(USERNAME_MAX_LEN));
-    create_user_table.replace("pswd_len", QString::number(PASSWORD_MAX_LEN));
+    // If no user table yet, create it.
+    QString s = "CREATE TABLE IF NOT EXISTS Users ("
+        "name char(%1) PRIMARY KEY, pswd char(%2))";
+    QString create_user_table = s.arg(
+        QString::number(USERNAME_MAX_LEN), QString::number(PASSWORD_MAX_LEN));
     qDebug() << create_user_table << qry->exec(create_user_table);
+    // WONTDO: visual feedback, like initial message box.
 
-    // If there's no user yet, insert a default one.
+    // If no user yet, add a default one to login.
     QString count_users = "SELECT COUNT(*) FROM Users";
     qDebug() << count_users << qry->exec(count_users);
     if (qry->first() && !qry->value(0).toInt())
     {
-        QString insert_default_user = "INSERT INTO Users VALUES (name, pswd)";
-        insert_default_user.replace("name", DEFAULT_USERNAME);
-        insert_default_user.replace("pswd", DEFAULT_PASSWORD);
-        qDebug() << insert_default_user << qry->exec();
+        QString s = "INSERT INTO Users VALUES ('%1', '%2')";
+        QString add_default_user = s.arg(DEFAULT_USERNAME, DEFAULT_PASSWORD);
+        qDebug() << add_default_user << qry->exec();
     }
 }
 
@@ -44,10 +44,8 @@ void LoginDialog::on_loginButton_clicked()
     QString password = ui->lineEditPassword->text();
 
     // Validate input user info against user table in database
-    QString validate_user = "SELECT COUNT(*) FROM Users "
-        "WHERE name = 'username' AND pswd = 'password'";
-    validate_user.replace("username", username);
-    validate_user.replace("password", password);
+    QString s = "SELECT COUNT(*) FROM Users WHERE name='%1' AND pswd='%2'";
+    QString validate_user = s.arg(username, password);
     qDebug() << validate_user << qry->exec(validate_user);
     if (qry->first() && qry->value(0).toInt()) accept();
     // WONTDO: Exceoption when input user info incorrect
