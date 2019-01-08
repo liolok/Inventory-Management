baa27b544aa69dc2c243153632528d6a73b4b8b9
liolok <liolokit@gmail.com>
2019-01-09 00:52:20 +0800

    add constant for items

--- const.h
+++ const.h
@@ -5,14 +5,21 @@
 #include <QtDebug>
 #include <QSqlQuery>
 
+// Database
+const QString DB_TYPE = "QSQLITE";
+const QString DB_PATH = "./database.sqlite";
+
+// User
 const unsigned int USERNAME_MAX_LEN = 16;
 const unsigned int PASSWORD_MAX_LEN = 16;
-
 // Note that length must be less than max length above.
 const QString DEFAULT_USERNAME = "admin";
 const QString DEFAULT_PASSWORD = "admin";
 
-const QString DB_TYPE = "QSQLITE";
-const QString DB_PATH = "./database.sqlite";
+// Item
+const unsigned int ITEMNAME_MAX_LEN = 16;
+const unsigned int CATEGORY_MAX_LEN = 16;
+// This value should be quite small to avoid overflow.
+const unsigned int INVENTORY_IO_MAX = 1000;
 
 #endif // CONST_H
